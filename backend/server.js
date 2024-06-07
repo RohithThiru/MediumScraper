11c77fa9d9ae0fc7866c52062e7825d8c7792107
Rohith T <142724079+RohithThiru@users.noreply.github.com>
2024-06-07 08:43:55 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,6 @@ async function searchMedium(query) {
         }
     }
 }
-app.get('/', async (req, res) => {res.json('hello')}
 
 app.get('/search', async (req, res) => {
     const query = req.query.q;
